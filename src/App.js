d66692a1a184aa27dded35230756f90562a07e5e
J.C. Beneyto <jnk.beneyto@gmail.com>
2020-07-02 18:49:30 +0200

    adding Image styled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { Fragment } from "react";
 import Header from "./components/header";
+import styled from "@emotion/styled";
+
+import image from "./img/cryptocurrency.png";
+
+const Imagecontainer = styled.img`
+	max-width: 100%;
+`;
 
 function App() {
 	return (
@@ -7,8 +14,12 @@ function App() {
 			<Header title="Cotizador de Criptomonedas" />
 			<div className="container">
 				<div className="row">
-					<div className="col-sm-12 col-md-6">logo</div>
-					<div className="col-sm-12 col-md-6">form</div>
+					<div className="col-sm-12 col-md-6">
+						<Imagecontainer src={image} alt="imagen" />
+					</div>
+					<div className="col-sm-12 col-md-6">
+						<h3>Selecciona moneda:</h3>{" "}
+					</div>
 				</div>
 			</div>
 		</Fragment>

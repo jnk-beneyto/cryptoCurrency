b312b6e77b1d9d0685d9f676b564aeae42d65662
J.C. Beneyto <jnk.beneyto@gmail.com>
2020-07-04 19:19:57 +0200

    creating error component

---
+++ src/components/error.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = ({ message }) => {
+	return (
+		<div className="alert alert-danger">
+			<h4>Selecciona todos los valores</h4>
+		</div>
+	);
+};
+
+export default Error;
